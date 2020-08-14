5b1dc75d7dcbb4dd40a75e44b576ec14020dbbda
佐々木達也 <sasakitatsuya@MacBook-Pro-2.local>
2020-08-14 22:41:25 +0900

    CSSモジュール仕様に変更

--- src/components/HeaderCalender.js
+++ src/components/HeaderCalender.js
@@ -1,8 +1,10 @@
 import React from "react";
 import HeaderCalenderItem from "./HeaderCalenderItem";
+import style from "./HeaderCalender.css";
+import commonStyle from "./Common.css";
 
 const HeaderCalender = () => (
-  <ul className="headerCalender fl_end">
+  <ul className={`${style.headerCalender} ${commonStyle.fl_end}`}>
     <HeaderCalenderItem />
     <HeaderCalenderItem />
     <HeaderCalenderItem />
