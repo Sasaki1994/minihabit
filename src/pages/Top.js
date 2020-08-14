f855e6d1c978db19372d7f9cae20a5275da8236e
pinebook0515 <t.pinebook0515@gmail.com>
2020-08-14 18:42:20 +0900

    一覧のスタイルを調整

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,24 +1,31 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import HabitListItem from "../components/HabitListItem";
-import {useSelector} from "react-redux";
+import HeaderCalender from "../components/HeaderCalender";
+import { useSelector } from "react-redux";
 
 const Top = () => {
-    const habitList = useSelector(state => state.habits);
-    const user = useSelector(state => state.user);
+  const habitList = useSelector((state) => state.habits);
+  const user = useSelector((state) => state.user);
 
-    return (
+  return (
     <div>
-        <h2>Top Page</h2>
-        <h3>{user.name}さん</h3>
-        <Link to={"/habits/new"}>登録ページ</Link>
-        <ul>
-            {habitList.map((habit) => (
-                <HabitListItem key={habit.name} name={habit.name} number={habit.number} unit={habit.unit} />
-            ))}
-        </ul>
+      <h2>Top Page</h2>
+      <h3>{user.name}さん</h3>
+      <Link to={"/habits/new"}>登録ページ</Link>
+      <HeaderCalender />
+      <ul className="habitList">
+        {habitList.map((habit) => (
+          <HabitListItem
+            key={habit.name}
+            name={habit.name}
+            number={habit.number}
+            unit={habit.unit}
+          />
+        ))}
+      </ul>
     </div>
-    );
+  );
 };
 
 export default Top;
--- src/pages/habits/New.js
+++ src/pages/habits/New.js
@@ -1,23 +1,28 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import AddHabit from "../../components/AddHabit";
 import HabitListItem from "../../components/HabitListItem";
-import {useSelector} from "react-redux";
-
+import { useSelector } from "react-redux";
 
 const New = () => {
-    const habitList = useSelector(state => state.habits)
-    return (
+  const habitList = useSelector((state) => state.habits);
+  return (
     <div>
       <h2>Register Page</h2>
       <Link to={"/"}>トップページ</Link>
       <AddHabit />
       <ul>
         {habitList.map((habit) => (
-          <HabitListItem key={habit.name} name={habit.name} number={habit.number} unit={habit.unit} />
+          <HabitListItem
+            key={habit.name}
+            name={habit.name}
+            number={habit.number}
+            unit={habit.unit}
+          />
         ))}
       </ul>
     </div>
-    );
+  );
 };
 
 export default New;
